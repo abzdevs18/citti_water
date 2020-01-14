817761b0c11be9ee411b57fc5805b7c13683b8f2
Clint.Abz <abz.devs@gmail.com>
2020-01-14 23:08:27 +0800

    New

--- included/order_deliver_confirm.php
+++ included/order_deliver_confirm.php
@@ -1,7 +1,9 @@
 <?php
 include_once ("db.php");
 
-$id = $_REQUEST['o_id'];
-$sql= mysqli_query($conn,"UPDATE orders SET status = 4 WHERE o_id = '$id'");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['o_id'];
+    $sql= mysqli_query($conn,"UPDATE orders SET status = 4 WHERE o_id = '$id'");
 
+}
 ?>
