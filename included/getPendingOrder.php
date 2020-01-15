74dfd3e5b194e9dbfb188ec99790ab384c35184c
Clint.Abz <abz.devs@gmail.com>
2020-01-15 11:33:04 +0800

    Jan166

--- included/getPendingOrder.php
+++ included/getPendingOrder.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$data = mysqli_query($conn, "SELECT * FROM `orders` WHERE `status` = 0");
 	$rest = array();
 	while ($row = mysqli_fetch_assoc($data)) {
-		array_push($rest,array('o_id'=>$row['o_id'],'o_bottle'=>$row['o_bottle'],'o_cost'=>$row['o_cost'],'date_of_order'=>$row['date_of_order'],'time_of_delivery'=>$row['time_of_delivery'],'Lng'=>$row['Lng'],'Lat'=>$row['Lat'],));
+		array_push($rest,array('o_status'=>$row['status'],'o_id'=>$row['o_id'],'o_bottle'=>$row['o_bottle'],'o_cost'=>$row['o_cost'],'date_of_order'=>$row['date_of_order'],'time_of_delivery'=>$row['time_of_delivery'],'Lng'=>$row['Lng'],'Lat'=>$row['Lat'],));
 	}
 
 	echo json_encode($rest);
